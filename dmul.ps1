f6af6c4d1ee7ca906ce9cc20a4e5ca804474c7b5
binarydesu <binary.fabian@gmail.com>
2020-02-22 17:28:49 +0100

    added copy function, code cleanup, script runs now in terminal path

--- dmul.ps1
+++ dmul.ps1
@@ -1,6 +1,5 @@
-
-#Get all direcotry items
-$direcotry = Get-Childitem -Path .\
+#Get all directory items
+$directory = Get-Childitem -LiteralPath $PWD
 
 #Regular expression to find countries = {"xxx", "yyy", "zzz"}
 $regex = "(\bcountries\b[\s][\=][\s][`{][\s]*){1}([\s]*[`"][A-Z]{3}[`"][`,]*[\s]*)+([\s]*[`}])"
@@ -13,30 +12,72 @@ $modules = ".*A-10A.*|.*A-10C.*|.*AJS37.*|.*AV8BNA.*|.*BF-109K-4.*|.*C-101.*|.*C
 
 
 #check if DCS is in the same directory as script
-if ($direcotry.Name -match 'DCS World') {
+if ($directory.FullName -match "DCS World") {
 	
 	#get full path to DSC World
-	$dcspath = Resolve-Path -Path $($direcotry | Where-Object {$_.Name -match 'DCS World'})
+	$dcspath = $directory | Where-Object {$_ -match "DCS World"} | Select-Object  -ExpandProperty FullName 
+	#path for copy option
+	$copypath = Join-Path -Path $PWD -ChildPath "DCS_Unlocked_Liveries" 
 
 	Write-Output "DSC found in: $dcspath"
 
 	#get description.lua file paths and only get those from modules
-	$luapaths = $(Get-ChildItem -Path $dcspath\Bazar\Liveries\* -Recurse -Filter "description.lua" | Where-Object {$_.FullName -match $modules} | ForEach-Object {$_.FullName})
-	$luapaths += $(Get-ChildItem -Path $dcspath\CoreMods\* -Recurse -Filter "description.lua" | Where-Object {$_.FullName -match $modules} | ForEach-Object {$_.FullName})
+	$luapaths = Get-ChildItem -LiteralPath $dcspath -Recurse -Filter "description.lua" | Where-Object {$_.FullName -match $modules} | ForEach-Object {$_.FullName}
 
 	#count entries
 	$count = $luapaths.Length
 
-	$save = Read-Host -Prompt "Do you want to change $count files in $dcspath ? (y/n)"
+	$change = Read-Host -Prompt "Change $count files in $dcspath\? (y/n) or Keep original and save modified files in $copypath ? (c/n)"
 
-	if ($save -eq 'y' -or $save -eq 'Y'){ 
-		#replace matched string
-		$luapaths | ForEach-Object  {$(Get-Content -LiteralPath "$_") -replace $regex, $regin | Set-Content -LiteralPath "$_"}
-	} else {
-		Write-Output "Exiting Script"
-		exit
+	Switch ($change){
+
+		#replace inside DCS folder
+		{$_ -match 'y' -or $_ -match 'Y'} {
+
+			$luapaths | ForEach-Object  {$(Get-Content -LiteralPath "$_") -replace $regex, $regin | Set-Content -LiteralPath "$_"}
+
+		}
+
+		#replace in new directory
+		{$_ -match 'c' -or $_ -match 'C'} {
+
+			for ($i = 0; $i -lt $count; $i++) {
+
+				#little check so the created fodlers can be put inside dcs main directory for overwrite
+				if ($luapaths[$i] -match [regex]::escape("\Bazar\")) {
+
+					$subfolderscount = 5
+
+				} else {
+
+					$subfolderscount = 7
+
+				}
+	
+				#ugly string replacement/construction to .\DCS_Unlocked_Liveries\*
+				$tmppath = $copypath + $(($luapaths[$i].Split("\") | Select-Object -Last $subfolderscount) -join "\")
+				
+				#create new fodler structure
+				New-Item -ItemType File -Path $tmppath -Force | Out-Null
+				
+				#write/fill luas
+				(Get-Content -LiteralPath $luapaths[$i]) -replace $regex, $regin | Set-Content -LiteralPath $tmppath
+
+			}
+
+		}
+
+		#user aborted
+		default {
+
+			Write-Output "Exiting Script"
+			exit
+
+		}
 	}
 
 } else {
-Write-Output "DCS not found :("
+
+Write-Output "DCS not found in this directory :("
+
 }
