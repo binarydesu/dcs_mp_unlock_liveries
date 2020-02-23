94792f849669f50ab7110c3994bde85c9a80026b
binarydesu <binary.fabian@gmail.com>
2020-02-23 19:59:10 +0100

    Complete Overhaul

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
-# DCS Unlock Liveries in Multiplayer 
-
+# DCS Unlock Liveries for Multiplayer script
 ## PowerShell script for modifying description.luas in dcs's livery folders
-Please Advise the possible problems further down! 
+
+Please advise possible problems further down! 
 Use at your own risk. 
 
-1. Open PowerShell by right clicking your Home Button and "Windows Powershell" 
-2. Navigate to the folder containing your DCS Install (use "cd" command) 
-3. Run the Script (e.g. D:\Downloads\dcs_mp_unlock_liveries\dmul.ps1) 
-4. Choose your option 
-5. Profit 
+1. Download .zip and unzip in any directory (e.g. D:\Downloads)
+2. Open PowerShell by right clicking your Home Button and select "Windows PowerShell" 
+3. If you want to modify the files inside your DCS directory run `D:\Downloads\dcs_mp_unlock_liveries\dmul.ps1 'C:\DCS World OpenBeta'`
+4. If you want copy the .luas into a separate folder add the save directory `D:\Downloads\dcs_mp_unlock_liveries\dmul.ps1 'C:\DCS World OpenBeta' 'D:\DCS_Liveries'`
+    * this will retain the DCS folder Structure e.g. `D:\DCSLiveries\CoreMods\aircraft\F14\Liveries\f-14b\Santa\description.lua`
+
   
-# Possible Problems: 
+## Possible Problems: 
 
 - if you can't run scripts: open PowerShell as Administrator and run `set-executionpolicy remotesigned` 
-- Only works of you have ONE DCS install where you run the script, also it needs to contain "DCS World" 
-- Copying files will override already existing files without prompt, make sure .\DCS_Unlocked_Liveries\ does not exist in your folder 
-- script also overrides default skins in MODs folder (nothing concerning but unnecessary) 
+- Copying files will override already existing files (with the same name) without prompt, make sure your folder is correct
+- some description.luas are already modified by default, those won't be copied/modified
+- ~~ Only works of you have ONE DCS install where you run the script, also it needs to contain "DCS World" ~~
+- ~~ script also overrides default skins in MODs folder (nothing concerning but unnecessary) ~~
 
-# ToDo 
+## ToDo 
 
-- [ ] Make it run with parameters for dcs path and save path 
+- [x] Make it run with parameters for dcs path and save path 
+- [ ] Handle Exception when no valid path is given
--- dmul.ps1
+++ dmul.ps1
@@ -1,83 +1,154 @@
-#Get all directory items
-$directory = Get-Childitem -LiteralPath $PWD
+#Argument handle
+param (
+	[parameter(
+		Mandatory = $true,
+		Position = 0,
+		HelpMessage = "Enter DCS Path for modifiying liveries")]
+	[ValidateCount(1, 1)]
+	[Alias("path", "dcs", "dp")]
+	[String[]]
+	$dcspath,
+	
+	[parameter(
+		Mandatory = $false,
+		Position = 1,
+		HelpMessage = "Enter Save Path for modified liveries")]
+	[ValidateCount(1, 1)]
+	[Alias("copy", "cp")]
+	[String[]]
+	$copypath = $false
+)
+
+#Check if directory has Bazar and CoreMods
+if (Test-Path -Path  $dcspath) {
+
+	#Get complete path in case of a relative path
+	$dcspath = Resolve-Path -LiteralPath $dcspath
+	#Get directory content
+	$directory = Get-Childitem -LiteralPath $dcspath
+	#Escaped path to Bazar
+	$bazaresc = [regex]::escape(($(Join-Path -Path $dcspath -ChildPath 'Bazar')))
+	#Escaped path to CoreMods
+	$coremodesc = [regex]::escape(($(Join-Path -Path $dcspath -ChildPath 'CoreMods')))
+
+	if (!($directory.FullName -match $bazaresc) -and ($directory.FullName -match $coremodesc)) {
+		Write-Output "'$dcspath' DCS not found in this directory"
+		exit
+	}
+}
+else {
+	Write-Output "'$dcspath' Invalid directory"
+	exit
+}
+
+#Validate CopyPath argument
+if ($copypath -ne $false) {
+
+	if (Test-Path -Path $copypath) {
+
+		$copypath = Resolve-Path -LiteralPath $copypath
+		Write-Output "'$copypath' Directory is valid and will be used to save modified .luas"
+	}
+	else {
+		Write-Output "'$copypath' Directory is not valid. Exiting Script"
+		exit
+	}
+}
 
 #Regular expression to find countries = {"xxx", "yyy", "zzz"}
-$regex = '(?m)(\bcountries\b[\s\S][\=][\s\S]*[\{][\s\S]*){1}([\s\S]*[\"][A-Z]{3}[\"][\,]*[\s\S]*)+([\s\S]*[\}])'
+$regex = '(?m)^(\bcountries\b[\s\S]*[\=][\s\S]*[\{][\s\S]*){1}([\s\S]*[\"][A-Z]*[\"][\,]*[\s\S]*)+([\s\S]*[\}])'
 
-#regular expression to insert above groups but as commentary
+#Regex to check if file is already modified
+$regcheck = '(?m)^([\-]{2}([\[]{2}|[\[]{0}))+[\s]*(\bcountries\b)+[\s\S]*([\]]{2})*'
+
+#regular expression to insert groups but as commentary
 $regin = "--[[ `$1 `n `t `$2 `n `$3 ]]"
 
 #Regex containing all modules
-$modules = '.*A-10A.*|.*A-10C.*|.*AJS37.*|.*AV8BNA.*|.*BF-109K-4.*|.*C-101.*|.*Christian Eagle II.*|.*f_a-18C.*|.*F-15C.*|.*F-16C.*|.*f-16c bl.50.*|.*F-5EF-86.*|.*F14.*|.*FA-18C.*|.*FW-190A8.*|.*FW-190D9.*|.*I-16.*|.*ka-50.*|.*L-39.*|.*M-2000C.*|.*Mi-8mt.*|.*MiG-15bis.*|.*MiG-19P.*|.*MiG-21bis.*|.*mig-29a.*|.*mig-29g.*|.*mig-29s.*|.*mirage 2000-5.*|.*P-51D.*|.*SA342.*|.*su-25.*|.*su-25t.*|.*su-27.*|.*su-33.*|.*uh-1h.*|.*YAK-52.*|.*JF-17.*|.*J-11A.*|.*ChinaAssetPack.*|.*SpitfireLFMkIX.*'
+$modules = '.*A-10A.*|.*A-10C.*|.*AJS37.*|.*AV8BNA.*|.*BF-109K-4.*|.*C-101.*|.*Christen Eagle II.*|.*f_a-18C.*|.*F-15C.*|.*F-16C.*|.*f-16c bl.50.*|.*F-5EF-86.*|.*F14.*|.*FA-18C.*|.*FW-190A8.*|.*FW-190D9.*|.*I-16.*|.*ka-50.*|.*L-39.*|.*M-2000C.*|.*Mi-8mt.*|.*MiG-15bis.*|.*MiG-19P.*|.*MiG-21bis.*|.*mig-29a.*|.*mig-29g.*|.*mig-29s.*|.*mirage 2000-5.*|.*P-51D.*|.*SA342.*|.*su-25.*|.*su-25t.*|.*su-27.*|.*su-33.*|.*uh-1h.*|.*YAK-52.*|.*JF-17.*|.*J-11A.*|.*ChinaAssetPack.*|.*SpitfireLFMkIX.*'
 
+Write-Output "DSC found in: $dcspath"
 
-#check if DCS is in the same directory as script
-if ($directory.FullName -match "DCS World") {
+#get description.lua file paths and only get those from modules and only those in Bazar and CoreMods
+$luapaths = Get-ChildItem -LiteralPath $dcspath -Recurse -Filter "description.lua" | 
+Where-Object { $_.FullName -match $modules } | 
+ForEach-Object { $_.FullName } | 
+Select-String -Pattern "$bazaresc", "$coremodesc"
+
+#count entries
+$count = $luapaths.Length
+
+#check if there is a copy path
+if ($copypath -ne $false) {
+	#ask user to continue
+	if ($(Read-Host -Prompt "Copy and Modify $count files in '$dcspath' to '$copypath' ? (y/n)") -notmatch 'y') { exit }
 	
-	#get full path to DSC World
-	$dcspath = $directory | Where-Object {$_ -match "DCS World"} | Select-Object  -ExpandProperty FullName 
-	#path for copy option
-	$copypath = Join-Path -Path $PWD -ChildPath "DCS_Unlocked_Liveries\" 
-
-	Write-Output "DSC found in: $dcspath"
-
-	#get description.lua file paths and only get those from modules
-	$luapaths = Get-ChildItem -LiteralPath $dcspath -Recurse -Filter "description.lua" | Where-Object {$_.FullName -match $modules} | ForEach-Object {$_.FullName}
-
-	#count entries
-	$count = $luapaths.Length
-
-	$change = Read-Host -Prompt "Change $count files in $dcspath\? (y/n) or Keep original and save modified files in $copypath ? (c/n)"
-
-	Switch ($change){
-
-		#replace inside DCS folder
-		{$_ -match 'y' -or $_ -match 'Y'} {
-
-			$luapaths | ForEach-Object  {$(Get-Content -LiteralPath "$_" -Raw) -replace $regex, $regin | Set-Content -LiteralPath "$_"}
-
+	#counter for already modified/not modified files
+	$modifiedcount = 0
+	$unmodifiedcount = 0
+	$nomatchcount = 0
+	for ($i = 0; $i -lt $count; $i++) {
+		#little check so the created folders can be put inside dcs main directory for overwrite
+		if ($luapaths[$i] -match $bazaresc) {
+			$subfolderscount = 5
 		}
-
-		#replace in new directory
-		{$_ -match 'c' -or $_ -match 'C'} {
-
-			for ($i = 0; $i -lt $count; $i++) {
-
-				#little check so the created folders can be put inside dcs main directory for overwrite
-				if ($luapaths[$i] -match [regex]::escape("\Bazar\")) {
-
-					$subfolderscount = 5
-
-				} else {
-
-					$subfolderscount = 7
-
-				}
-	
-				#ugly string replacement/construction to .\DCS_Unlocked_Liveries\*
-				$tmppath = $copypath + $(($luapaths[$i].Split("\") | Select-Object -Last $subfolderscount) -join "\")
-				
-				#create new folder structure
-				New-Item -ItemType File -Path $tmppath -Force | Out-Null
-				
-				#write/fill luas
-				(Get-Content -LiteralPath $luapaths[$i] -Raw) -replace $regex, $regin | Set-Content -LiteralPath $tmppath
-
-			}
-
+		elseif ($luapaths[$i] -match $coremodesc) {
+			$subfolderscount = 7
 		}
-
-		#user aborted
-		default {
-
-			Write-Output "Exiting Script"
+		else {
+			Write-Output "Something went wrong! Exiting"
 			exit
+		}
+		#ugly string replacement/construction to $CopyPath
+		$tmppath = Join-Path -Path $copypath -ChildPath $((($luapaths[$i]).Line.Split("\") | Select-Object -Last $subfolderscount) -join "\")
+				
+		#create new folder structure
+		New-Item -ItemType File -Path $tmppath -Force | Out-Null
+				
+		#check if already modified then write/fill luas
+		if ((Get-Content -LiteralPath $luapaths[$i] -Raw) -match $regcheck) {
+			$unmodifiedcount++
+		}
+		elseif ((Get-Content -LiteralPath $luapaths[$i] -Raw) -match $regex) {
+			(Get-Content -LiteralPath $luapaths[$i] -Raw) -replace $regex, $regin | Set-Content -LiteralPath $tmppath
+			$modifiedcount++
+		}
+		else {
+			$nomatchcount++
+		}
+	}	
+	Write-Output "Successfully copied and changed $modifiedcount files"
+}
+else {
+	#No copy path given, Ask user to continue
+	if ($(Read-Host -Prompt "Modify $count files in $dcspath ? (y/n)") -notmatch 'y') { exit }
 
+	#counter for already modified/not modified files
+	$modifiedcount = 0
+	$unmodifiedcount = 0
+	$nomatchcount = 0
+	for ($i = 0; $i -lt $count; $i++) {
+		
+		if ((Get-Content -LiteralPath $luapaths[$i] -Raw) -match $regcheck) {
+			$unmodifiedcount++
+			
+		}
+		elseif ((Get-Content -LiteralPath $luapaths[$i] -Raw) -match $regex) {
+			#Modify .lua files in DCS directory
+			$luapaths | ForEach-Object { $(Get-Content -LiteralPath "$_" -Raw) -replace $regex, $regin | Set-Content -LiteralPath "$_" }
+			$modifiedcount++			
+		}
+		else {
+			$nomatchcount++
 		}
 	}
+	Write-Output "Successfully changed $modifiedcount files"
+}
 
-} else {
 
-Write-Output "DCS not found in this directory :("
-
+if ($unmodifiedcount -gt 0) {
+	Write-Output "Seems like $unmodifiedcount files were already modified, didn't touch those"
+}
+if ($nomatchcount -gt 0) {
+	Write-Output "Seems like $nomatchcount had no country definition, didn't touch those"
 }
